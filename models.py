9812d4710cdc2b2afb535e70215cc20822a994f7
Larry Zhang <lzhang124@gmail.com>
2018-03-01 14:33:27 -0500

    smaller lr

--- models.py
+++ models.py
@@ -100,7 +100,7 @@ class UNet(BaseModel):
         self.model = Model(inputs=inputs, outputs=outputs)
 
     def _compile(self):
-        self.model.compile(optimizer=Adam(lr=1e-4),
+        self.model.compile(optimizer=Adam(lr=1e-5),
                            loss='binary_crossentropy',
                            metrics=[dice_coef])
 
