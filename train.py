d93f6508e37bcedc9e2fd01dae8f31213ad9da2c
Larry Zhang <lzhang124@gmail.com>
2018-04-09 16:55:17 -0400

    comment

--- train.py
+++ train.py
@@ -148,12 +148,12 @@ def seed_test(options):
     seed_files = glob.glob('data/labels/{}/{}_*_placenta.nii.gz'.format(number, number))
     predict_files = [file.replace('labels', 'raw').replace('_placenta', '') for file in seed_files]
     pred_gen = VolSliceGenerator(predict_files, seed_files, options.batch_size)
-    # model.predict(pred_gen, 'data/predict/{}/')
+    model.predict(pred_gen, 'data/predict/{}/')
 
-    # logging.info('Testing model.')
-    # test_gen = zip(pred_gen, VolumeGenerator(seed_files, None, options.batch_size, False))
-    # performance = model.test(test_gen)
-    # logging.info(performance)
+    logging.info('Testing model.')
+    test_gen = zip(pred_gen, VolumeGenerator(seed_files, None, options.batch_size, False))
+    performance = model.test(test_gen)
+    logging.info(performance)
 
     end = time.time()
     logging.info('total time: {}s'.format(end - start))
