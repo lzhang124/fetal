9d0cea67e3db202db85743fcac4adf17669f412d
Larry Zhang <lzhang124@gmail.com>
2018-05-01 13:21:28 -0400

    typo

--- train.py
+++ train.py
@@ -181,7 +181,7 @@ def run(options):
                                    gen_seed=gen_seed)
         a = aug_gen.next()
         print(a[0].shape)
-        save_vol(a[0], 'test_vol.nii.gz', rescale=True)
+        save_vol(a[0], 'test_vol.nii.gz', scale=True)
         save_vol(a[1], 'test_label.nii.gz')
         raise ValueError
         val_gen = VolumeGenerator(input_files,
