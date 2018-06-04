65c49d9bef966f3b810880430da8f5946c746902
Larry Zhang <lzhang124@gmail.com>
2018-06-04 12:53:24 -0400

    better combine

--- combine_brains.py
+++ combine_brains.py
@@ -5,11 +5,11 @@ import util
 
 for sample in ['043015', '051215', '061715', '062515', '081315', '083115', '110214', '112614', '122115', '122215']:
     for n in ['1', '49', '99', '149', '199', '249']:
-        files = glob.glob('data/labels/{}/{}_*_brains.nii.gz'.format(sample, n))
+        files = glob.glob('data/labels/{}/{}_{}_*_brains.nii.gz'.format(sample, sample, n))
 
         volume = np.zeros(util.shape(files[0]))
         header = util.header(files[0])
         for file in files:
-            volume += util.read_vol(filename)
+            volume += util.read_vol(file)
 
-        util.save_vol(volume, 'data/labels/{}/{}_all_brains.nii.gz'.format(sample, n), header)
+        util.save_vol(volume, 'data/labels/{}/{}_{}_all_brains.nii.gz'.format(sample, sample, n), header)
