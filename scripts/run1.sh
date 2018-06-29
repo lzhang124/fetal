57ba69724058fd5486da9e139ee8c99fd831130f
Larry Zhang <lzhang124@gmail.com>
2018-06-29 11:07:06 -0400

    testing?

--- scripts/run1.sh
+++ scripts/run1.sh
@@ -7,4 +7,4 @@ python_exe=/data/vision/polina/shared_software/anaconda3-4.3.1/envs/keras/bin/py
 
 cd ${placenta_dir}
 
-nohup ${python_exe} train.py "$@ --gpu 1" > nohup1.out 2> nohup1.err < /dev/null &
+nohup ${python_exe} train.py "--gpu 1 $@" > nohup1.out 2> nohup1.err < /dev/null &
