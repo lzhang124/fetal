aacb8b67e248f940711efb2ee37a9a1886e0e06e
Larry Zhang <lzhang124@gmail.com>
2018-07-01 14:56:56 -0400

    space

--- scripts/run0.sh
+++ scripts/run0.sh
@@ -7,5 +7,5 @@ python_exe=/data/vision/polina/shared_software/anaconda3-4.3.1/envs/keras/bin/py
 
 cd ${placenta_dir}
 
-args="--gpu0"
+args="--gpu 0"
 nohup ${python_exe} train.py ${args} "$@" > nohup0.out 2> nohup0.err < /dev/null &
