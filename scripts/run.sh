9af28d499e479df4876a495146d7f78c7dac9396
Larry Zhang <lzhang124@gmail.com>
2018-07-01 15:59:14 -0400

    shift

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,5 +8,6 @@ python_exe=/data/vision/polina/shared_software/anaconda3-4.3.1/envs/keras/bin/py
 cd ${placenta_dir}
 
 gpu=$1
+args="--gpu $1"
 shift
 nohup ${python_exe} train.py ${args} "$@" > nohup${gpu}.out 2> nohup${gpu}.err < /dev/null &
