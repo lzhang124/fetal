b71a4e15c9e1b51bf56ff019ea2c9cd84bde22ad
Larry Zhang <lzhang124@gmail.com>
2018-07-23 10:40:23 -0400

    new scp script

--- scripts/scp.sh
+++ scripts/scp.sh
@@ -9,6 +9,6 @@ placenta_dir=/data/vision/polina/projects/placenta_segmentation/
 
 remote_ssh=${username}@${remote_machine}.csail.mit.edu
 
-run_cmd="scp -r ${remote_ssh}:${placenta_dir}/data/predict/* ./data/predict/"
+run_cmd="rsync -avP ${remote_ssh}:${placenta_dir}/data/predict/* ./data/predict/"
 
 eval ${run_cmd}
