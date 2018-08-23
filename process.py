3c5d5a4014a69e9995ab32e687665fc6072d12bd
Larry Zhang <lzhang124@gmail.com>
2018-08-23 11:33:06 -0400

    reshape with odds

--- process.py
+++ process.py
@@ -11,11 +11,11 @@ def crop(vol):
         raise ValueError('The input shape {} is not supported.'.format(vol.shape))
 
     # convert to target shape
-    dx = abs(constants.SHAPE[0] - vol.shape[0]) // 2
-    dy = abs(constants.SHAPE[1] - vol.shape[1]) // 2
-    dz = abs(constants.SHAPE[2] - vol.shape[2]) // 2
+    dx = (constants.SHAPE[0] - vol.shape[0]) // 2
+    dy = (constants.SHAPE[1] - vol.shape[1]) // 2
+    dz = (constants.SHAPE[2] - vol.shape[2]) // 2
     
-    resized = vol[dx:-dx, dy:-dy, dz:-dz]
+    resized = vol[dx:dx+constants.SHAPE[0], dy:dy+constants.SHAPE[0], dz:dz+constants.SHAPE[0]]
     if resized.shape != constants.SHAPE:
         raise ValueError('The resized shape {shape} '
                          'does not match the target '
@@ -46,11 +46,14 @@ def uncrop(vol, shape):
         raise ValueError('The target shape {} is not supported.'.format(shape))
 
     # convert to original shape
-    dx = abs(shape[0] - vol.shape[0]) // 2
-    dy = abs(shape[1] - vol.shape[1]) // 2
-    dz = abs(shape[2] - vol.shape[2]) // 2
+    dx = (vol.shape[0] - shape[0]) // 2
+    dy = (vol.shape[1] - shape[1]) // 2
+    dz = (vol.shape[2] - shape[2]) // 2
 
-    resized = np.pad(vol, ((dx, dx), (dy, dy), (dz, dz), (0, 0)), 'constant')
+    resized = np.pad(vol, ((dx, vol.shape[0]-dx),
+                           (dy, vol.shape[1]-dy),
+                           (dz, vol.shape[2]-dz),
+                           (0, 0)), 'constant')
     if resized.shape != shape:
         raise ValueError('The resized shape {shape} '
                          'does not match the target '
