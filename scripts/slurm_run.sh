47bae008d5d334f061cd601bccbd9f6c59b0d091
Larry Zhang <lzhang124@gmail.com>
2018-09-04 14:03:01 -0400

    test cuda

--- scripts/slurm_run.sh
+++ scripts/slurm_run.sh
@@ -2,6 +2,7 @@
 
 export CUDA_HOME=/data/vision/polina/shared_software/anaconda3-4.3.1/envs/keras/cuda
 export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
+echo $CUDA_HOME
 
 placenta_dir=/data/vision/polina/projects/placenta_segmentation/
 python_exe=/data/vision/polina/shared_software/anaconda3-4.3.1/envs/keras/bin/python
