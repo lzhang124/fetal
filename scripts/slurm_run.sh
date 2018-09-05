484b305e7f9cff7c19c9b7a1a25914dd251c5d0c
Larry Zhang <lzhang124@gmail.com>
2018-09-05 15:01:25 -0400

    update slurm run

--- scripts/slurm_run.sh
+++ scripts/slurm_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CUDA_HOME=/data/vision/polina/shared_software/anaconda3-4.3.1/envs/larry/cuda
-export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
 export HOME=/data/vision/polina/projects/placenta_segmentation/
+export CUDA_HOME=$HOME/cuda
+export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
 
-placenta_dir=/data/vision/polina/projects/placenta_segmentation/
+placenta_dir=$HOME
 python_exe=${placenta_dir}/venv/bin/python
 
 ###################
