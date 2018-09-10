fa3d0979db31ae26a537c16a98745666d5be8355
Larry Zhang <lzhang124@gmail.com>
2018-09-10 15:16:08 -0400

    rm metabar

--- train.py
+++ train.py
@@ -59,7 +59,6 @@ parser.add_argument('--part',
                     metavar='PART',
                     dest='part', type=int)
 parser.add_argument('--tensorboard',
-                    metavar='TENSORBOARD',
                     dest='tensorboard', action='store_true')
 options = parser.parse_args()
 
