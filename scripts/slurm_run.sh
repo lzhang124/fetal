88f68740794517899702545010c91e3d9b824cd3
Larry Zhang <lzhang124@gmail.com>
2018-09-24 14:54:46 -0400

    slurm run

--- scripts/slurm_run.sh
+++ scripts/slurm_run.sh
@@ -5,7 +5,6 @@ placenta_dir=/data/vision/polina/projects/placenta_segmentation
 ###################
 
 cd ${placenta_dir}
-for i in {1..3}
-do
-    srun -p gpu -t 20:00:00 --mem-per-cpu 1 --gres=gpu:1 -J part$i -o run$i.out -e run$i.err scripts/run.sh --part $i "$@" &
-done
+i=$1
+shift
+srun -p gpu -t 20:00:00 --mem-per-cpu 1 --gres=gpu:1 -J run${i} -o run${i}.out -e run${i}.err scripts/run.sh "$@" &
