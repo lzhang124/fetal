a5c015c66e98a7d04d5a2bb0554d3eae2e9fb3b3
Larry Zhang <lzhang124@gmail.com>
2018-09-24 13:07:45 -0400

    update

--- scripts/split_nifti.sh
+++ scripts/split_nifti.sh
@@ -6,4 +6,4 @@ placenta_dir=/data/vision/polina/projects/placenta_segmentation
 
 scripts/download_data.sh nifti/$1/
 python -B split_nifti.py data/nifti/$1/
-scripts/upload_data.sh originals/$1/
+scripts/upload_data.sh raw/$1/
