a583a70bacdf84a16adfac70dd759a5f2627883e
Larry Zhang <lzhang124@gmail.com>
2018-10-03 12:15:23 -0400

    add time option to slurm run

--- scripts/slurm_run.sh
+++ scripts/slurm_run.sh
@@ -7,4 +7,6 @@ placenta_dir=/data/vision/polina/projects/placenta_segmentation
 cd ${placenta_dir}
 i=$1
 shift
-srun -p gpu -t 20:00:00 --mem-per-cpu 1 --gres=gpu:1 -J run${i} -o run${i}.out -e run${i}.err scripts/run.sh "$@" &
+t=$1
+shift
+srun -p gpu -t ${t}:00:00 --mem-per-cpu 1 --gres=gpu:1 -J run${i} -o run${i}.out -e run${i}.err scripts/run.sh "$@" &
