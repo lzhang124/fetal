2ded016d73dba943bd7f88829455f5257c18d3c6
Larry Zhang <lzhang124@gmail.com>
2018-11-13 15:31:32 -0500

    revert dot

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ def weighted_crossentropy(weight=None, boundary_weight=None, pool=5):
         cross_entropy = K.stack([-(y_true * K.log(y_pred)), -((1 - y_true) * K.log(1 - y_pred))],
                                 axis=0)
         print(cross_entropy)
-        loss = K.dot(w, cross_entropy)
+        loss = w * cross_entropy
         print(loss)
 
         if boundary_weight is not None:
