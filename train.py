4d252d11ba9160c52b7a63f082f557ca2d1453e3
Larry Zhang <lzhang124@gmail.com>
2018-12-06 00:14:13 -0500

    predictions

--- train.py
+++ train.py
@@ -81,32 +81,40 @@ def main(options):
     val_label_files = [f'data/labels/{sample}/{sample}_0_{organ}.nii.gz' for sample in val]
     val_gen = VolumeGenerator(val_files, label_files=val_label_files, label_types=label_types)
 
-    pred_files = glob.glob('data/raw/*/*.nii.gz')
-    pred_gen = VolumeGenerator(pred_files, tile_inputs=True)
-
     test_files = [f'data/raw/{sample}/{sample}_0000.nii.gz' for sample in test]
     test_label_files = [f'data/labels/{sample}/{sample}_0_{organ}.nii.gz' for sample in test]
+    pred_gen = VolumeGenerator(test_files, tile_inputs=True)
     test_gen = VolumeGenerator(test_files, label_files=test_label_files, label_types=label_types)
 
     logging.info('Creating model.')
     shape = constants.SHAPE
     model = MODELS[options.model](shape, name=options.name, filename=options.model_file, weights=weights)
 
-    logging.info('Training model.')
-    model.train(train_gen, val_gen, options.epochs)
+    # logging.info('Training model.')
+    # model.train(train_gen, val_gen, options.epochs)
 
-    logging.info('Making predictions.')
-    model.predict(pred_gen, f'data/predict/{options.name}/')
+    # logging.info('Making predictions.')
+    # model.predict(pred_gen, f'data/predict/{options.name}/')
 
-    logging.info('Testing model.')
-    metrics = model.test(test_gen)
-    logging.info(metrics)
-    dice = {}
-    for i in range(len(test)):
-        sample = test[i]
-        dice[sample] = util.dice_coef(util.read_vol(test_label_files[i]), util.read_vol(f'data/predict/{options.name}/{sample}_0000.nii.gz'))
-    logging.info(metrics)
-    logging.info(np.mean(list(metrics.values())))
+    # logging.info('Testing model.')
+    # metrics = model.test(test_gen)
+    # logging.info(metrics)
+    # dice = {}
+    # for i in range(len(test)):
+    #     sample = test[i]
+    #     dice[sample] = util.dice_coef(util.read_vol(test_label_files[i]), util.read_vol(f'data/predict/{options.name}/{sample}_0000.nii.gz'))
+    # logging.info(metrics)
+    # logging.info(np.mean(list(metrics.values())))
+
+    for folder in glob.glob('data/raw/*'):
+        try:
+            sample = folder.split('/')[-1]
+            pred_files = glob.glob(f'data/raw/{sample}/{sample}_*.nii.gz')
+            pred_gen = VolumeGenerator(pred_files, tile_inputs=True)
+            model.predict(pred_gen, f'data/predict/{options.name}/{sample}/')
+        except Exception as e:
+            logging.error(f'ERROR during {sample}:')
+            logging.error(e.message)
 
     end = time.time()
     logging.info(f'total time: {datetime.timedelta(seconds=(end - start))}')
