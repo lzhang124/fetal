75d63d67fe08ef94da77f63347e29879a077dab1
Larry Zhang <lzhang124@gmail.com>
2018-12-10 10:46:46 -0500

    stuff

--- remove_artifacts.py
+++ remove_artifacts.py
@@ -9,32 +9,40 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('model', type=str)
+parser.add_argument('--metrics', action='store_true')
 options = parser.parse_args()
 
 
-def main(model):
-    old_metrics = {}
-    new_metrics = {}
+def main(model, metrics):
+    if metrics:
+        old_metrics = {}
+        new_metrics = {}
+        files = f'data/predict/{model}/*_0000.nii.gz'
+    files = f'data/predict/{model}/*/*.nii.gz'
 
-    for file in glob.glob(f'data/predict/{model}/*_0000.nii.gz'):
-        sample = os.path.basename(file).split('_')[0]
+    for file in glob.glob(files):
+        sample, i = os.path.basename(file).split('_')
+        print(sample)
+        i = i[:4]
         header = util.header(file)
         volume = util.read_vol(file)
-        label = util.read_vol(glob.glob(f'data/labels/{sample}/{sample}_0_all_brains.nii.gz')[0])
-
-        old_metrics[sample] = util.dice_coef(label, volume)
+        if metrics:
+            label = util.read_vol(glob.glob(f'data/labels/{sample}/{sample}_0_all_brains.nii.gz')[0])
+            old_metrics[sample] = util.dice_coef(label, volume)
 
         n = 2 if sample in constants.TWINS else 1
         volume = remove_artifacts(volume, n)
 
-        new_metrics[sample] = util.dice_coef(label, volume)
+        if metrics:
+            new_metrics[sample] = util.dice_coef(label, volume)
 
-        os.makedirs(f'data/predict_cleaned/{model}/', exist_ok=True)
-        util.save_vol(volume, f'data/predict_cleaned/{model}/{sample}_0000.nii.gz', header)
+        os.makedirs(f'data/predict_cleaned/{model}/{sample}', exist_ok=True)
+        util.save_vol(volume, f'data/predict_cleaned/{model}/{sample}/{sample}_{i}.nii.gz', header)
 
-    print('before: ', old_metrics)
-    print('after: ', new_metrics)
+    if metrics:
+        print('before: ', old_metrics)
+        print('after: ', new_metrics)
 
 
 if __name__ == '__main__':
-    main(options.model)
+    main(options.model, options.metrics)
---
+++ temp.py
@@ -0,0 +1,24 @@
+import util
+import glob
+import os
+import numpy as np
+
+files = glob.glob('data/predict_cleaned/unet3000/*')
+samples = [i.split('/')[-1] for i in files]
+
+for s in samples:
+    f = f'data/predict_cleaned/unet3000/{s}/{s}_0000.nii.gz'
+    if len(glob.glob(f)) == 0:
+        continue
+    header = util.header(f)
+    n = len(glob.glob(f'data/predict_cleaned/unet3000/{s}/{s}_*.nii.gz'))
+    h, w, d, _ = util.shape(f)
+    slice_shape = (h, w, n)
+    slices = [np.zeros(slice_shape) for i in range(d)]
+    for i in range(n):
+        a = util.read_vol(f'data/predict_cleaned/unet3000/{s}/{s}_{str(i).zfill(4)}.nii.gz')
+        for j in range(d):
+            slices[j][:,:,i] = a[:,:,j,0]
+    for i in range(d):
+        os.makedirs(f'data/slices/{s}/', exist_ok=True)
+        util.save_vol(slices[i], f'data/slices/{s}/{s}_{i}.nii.gz', header)
