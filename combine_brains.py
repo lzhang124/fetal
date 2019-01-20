aa42a571113bcafba5d861fdbac8e122b67fd305
Larry Zhang <lzhang124@gmail.com>
2019-01-20 15:47:03 -0500

    fix brains

--- combine_brains.py
+++ combine_brains.py
@@ -1,14 +1,14 @@
-import constants
 import glob
 import numpy as np
 import util
 
 
-for sample in constants.SAMPLES:
+samples = [i.split('/')[-1] for i in glob.glob('data/labels/*')]
+for sample in samples:
     files = glob.glob(f'data/labels/{sample}/{sample}_*_brain.nii.gz')
     frame = files[0].split('_')[1]
 
-    volume = np.zeros(util.shape(f'data/raw/{sample}/{sample}_0.nii.gz'))
+    volume = np.zeros(util.shape(f'data/raw/{sample}/{sample}_{str(frame).zfill(4)}.nii.gz'))
     header = util.header(files[0])
     for file in files:
         volume += util.read_vol(file)
