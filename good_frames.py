a20ef3d8c67da7204874feec339522176b1998be
Larry Zhang <lzhang124@gmail.com>
2019-02-08 13:13:33 -0500

    fix frames

--- good_frames.py
+++ good_frames.py
@@ -4,6 +4,7 @@ import util
 import pickle
 
 samples = [i.split('/')[-1] for i in glob.glob('data/predict_cleaned/unet3000/*')]
+good_frames = {}
 
 for s in sorted(samples):
     print(s)
@@ -22,8 +23,6 @@ for s in sorted(samples):
             dif = abs(volume - curr_vol)
             if dif / volume <= 0.1 and util.dice_coef(vol, seg) >= 0.8:
                 if prev is not None and abs(curr_vol - prev_vol) / prev_vol <= 0.05 and util.dice_coef(prev, seg) >= 0.9:
-                    if i == 1:
-                        frames.append(0)
                     frames.append(i)
             prev = seg
             prev_vol = curr_vol
@@ -37,13 +36,13 @@ for s in sorted(samples):
             dif = abs(volume - curr_vol)
             if dif / volume <= 0.1:
                 if prev is not None and abs(curr_vol - prev_vol) / prev_vol <= 0.05 and util.dice_coef(prev, seg) >= 0.9:
-                    if i == 1:
-                        frames.append(0)
                     frames.append(i)
             prev = seg
             prev_vol = curr_vol
-    print(len(frames))
+    print(len(frames)/len(segs))
+    if len(frames)/len(segs) > 0.6:
+        good_frames[s] = frames
 
-print(frames)
+print(good_frames)
 with open('good_frames.p', 'wb') as f:
-    pickle.dump(frames, f)
+    pickle.dump(good_frames, f)
