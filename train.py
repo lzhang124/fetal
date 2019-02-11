7c16aa62d9b8bb3f214ef7e757cfd8d300802ad2
Larry Zhang <lzhang124@gmail.com>
2019-02-11 13:17:39 -0500

    predict all option

--- train.py
+++ train.py
@@ -5,33 +5,24 @@ logging.basicConfig(level=logging.INFO)
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument('--model',
-                    metavar='Model',
-                    help='Model',
+                    help='Model architecture',
                     dest='model', type=str, required=True)
 parser.add_argument('--organ',
-                    metavar='ORGAN',
                     help='Organ to segment',
                     dest='organ', type=str, required=True)
 parser.add_argument('--epochs',
-                    metavar='EPOCHS',
                     help='Training epochs',
                     dest='epochs', type=int, default=1000)
 parser.add_argument('--name',
-                    metavar='MODEL_NAME',
                     help='Name of model',
                     dest='name', type=str, required=True)
 parser.add_argument('--model-file',
-                    metavar='MODEL_FILE',
                     help='Pretrained model file',
                     dest='model_file', type=str)
-parser.add_argument('--gpu',
-                    metavar='GPU',
-                    help='Which GPU to use',
-                    dest='gpu', type=str)
+parser.add_argument('--predict-all',
+                    help='Predict all samples',
+                    dest='predict_all', action='store_true')
 options = parser.parse_args()
-
-if options.gpu:
-    os.environ['CUDA_VISIBLE_DEVICES'] = options.gpu
 
 import constants
 import datetime
@@ -90,32 +81,34 @@ def main(options):
     shape = constants.SHAPE
     model = MODELS[options.model](shape, name=options.name, filename=options.model_file, weights=weights)
 
-    # logging.info('Training model.')
-    # model.train(train_gen, val_gen, options.epochs)
+    if options.predict_all:
+        for folder in glob.glob('data/raw/*'):
+            try:
+                sample = folder.split('/')[-1]
+                logging.info(f'{sample}..............................')
+                pred_files = glob.glob(f'data/raw/{sample}/{sample}_*.nii.gz')
+                pred_gen = VolumeGenerator(pred_files, tile_inputs=True)
+                model.predict(pred_gen, f'data/predict/{options.name}/{sample}/')
+            except Exception as e:
+                logging.error(f'ERROR during {sample}:')
+                logging.error(e)
 
-    # logging.info('Making predictions.')
-    # model.predict(pred_gen, f'data/predict/{options.name}/')
+    else:
+        logging.info('Training model.')
+        model.train(train_gen, val_gen, options.epochs)
 
-    # logging.info('Testing model.')
-    # metrics = model.test(test_gen)
-    # logging.info(metrics)
-    # dice = {}
-    # for i in range(len(test)):
-    #     sample = test[i]
-    #     dice[sample] = util.dice_coef(util.read_vol(test_label_files[i]), util.read_vol(f'data/predict/{options.name}/{sample}_0000.nii.gz'))
-    # logging.info(metrics)
-    # logging.info(np.mean(list(metrics.values())))
+        logging.info('Making predictions.')
+        model.predict(pred_gen, f'data/predict/{options.name}/')
 
-    for folder in glob.glob('data/raw/*'):
-        try:
-            sample = folder.split('/')[-1]
-            logging.info(f'{sample}..............................')
-            pred_files = glob.glob(f'data/raw/{sample}/{sample}_*.nii.gz')
-            pred_gen = VolumeGenerator(pred_files, tile_inputs=True)
-            model.predict(pred_gen, f'data/predict/{options.name}/{sample}/')
-        except Exception as e:
-            logging.error(f'ERROR during {sample}:')
-            logging.error(e)
+        logging.info('Testing model.')
+        metrics = model.test(test_gen)
+        logging.info(metrics)
+        dice = {}
+        for i in range(len(test)):
+            sample = test[i]
+            dice[sample] = util.dice_coef(util.read_vol(test_label_files[i]), util.read_vol(f'data/predict/{options.name}/{sample}_0000.nii.gz'))
+        logging.info(metrics)
+        logging.info(np.mean(list(metrics.values())))
 
     end = time.time()
     logging.info(f'total time: {datetime.timedelta(seconds=(end - start))}')
