894a5b702d107d38703fa6d0f7af9947b87fd824
Larry Zhang <lzhang124@gmail.com>
2019-02-13 16:53:57 -0500

    print

--- image3d.py
+++ image3d.py
@@ -345,6 +345,7 @@ class VolumeIterator(Iterator):
     def __init__(self, x, y, image_transformer,
                  batch_size=32, shuffle=True, seed=None):
         self.x = x
+        print(type(x[0]))
         if type(x[0]) is not str and any(x[i].ndim != 4 for i in range(len(x))):
             raise ValueError('Each volume of the input data for '
                              '`VolumeIterator` should have rank 4.')
