11e24a4394f857b20bcd59fbbc76606c19eb1699
Larry Zhang <lzhang124@gmail.com>
2019-02-13 20:26:03 -0500

    more cpu mem

--- scripts/slurm_run.sh
+++ scripts/slurm_run.sh
@@ -8,4 +8,4 @@ cd ${placenta_dir}
 name=$1
 shift
 rm logs/${name}/*
-srun -p gpu -t 20:00:00 --mem-per-cpu 1 --gres=gpu:1 -J ${name} -o ${name}.out -e ${name}.err scripts/run.sh "$@" --name ${name} &
+srun -p gpu -t 20:00:00 --mem-per-cpu 4 --gres=gpu:1 -J ${name} -o ${name}.out -e ${name}.err scripts/run.sh "$@" --name ${name} &
