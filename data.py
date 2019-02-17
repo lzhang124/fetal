47e9c1ab9454793d8054a85717794181ed3462ec
Larry Zhang <lzhang124@gmail.com>
2019-02-16 20:59:44 -0500

    print

--- data.py
+++ data.py
@@ -36,6 +36,7 @@ class AugmentGenerator(VolumeIterator):
                 self.labels = [preprocess(file) for file in label_files]
         elif concat_files is not None:
             self.inputs = np.stack((self.inputs, *concat_files))
+            print(self.inputs.shape)
 
         self.label_types = label_types
         self.load_files = load_files
