d957c279f1a4d6265633d061559d1aa0965f85b3
Larry Zhang <lzhang124@gmail.com>
2019-02-19 15:51:47 -0500

    as array

--- data.py
+++ data.py
@@ -32,8 +32,8 @@ class DataGenerator(Iterator):
             if label_files is not None:
                 self.labels = [preprocess(file, resize=self.resize, tile=self.tile_inputs) for file in self.label_files]
             if self.tile_inputs:
-                self.inputs = np.reshape(self.inputs, (-1,) + self.inputs.shape[-4:])
-                self.labels = np.reshape(self.labels, (-1,) + self.labels.shape[-4:])
+                self.inputs = np.reshape(self.inputs, (-1,) + np.asarray(self.inputs).shape[-4:])
+                self.labels = np.reshape(self.labels, (-1,) + np.asarray(self.labels).shape[-4:])
         if augment:
             self.image_transformer = ImageTransformer(rotation_range=90.,
                                              shift_range=0.1,
