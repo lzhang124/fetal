84cbbbf588e2c1ef663d13ce66a52cada82d1f45
Larry Zhang <lzhang124@gmail.com>
2019-02-19 14:41:32 -0500

    another remove

--- scripts/slurm_run.sh
+++ scripts/slurm_run.sh
@@ -7,5 +7,6 @@ placenta_dir=/data/vision/polina/projects/placenta_segmentation
 cd ${placenta_dir}
 name=$1
 shift
+rm models/${name}/*
 rm logs/${name}/*
 srun -p gpu -t 20:00:00 --mem-per-cpu 16 --gres=gpu:1 -J ${name} -o ${name}.out -e ${name}.err scripts/run.sh "$@" --name ${name} &
