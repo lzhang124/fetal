1bb17120bc74b2bf48040d48b6a3a28820a84cad
Larry Zhang <lzhang124@gmail.com>
2019-02-19 19:06:57 -0500

    setup for temporal

--- train.py
+++ train.py
@@ -86,60 +86,43 @@ def main(options):
 
         # logging.info('Creating data generators.')
         # label_types = LABELS[options.model]
-        # train_for = []
-        # train_rev = []
-        # train_label_for = []
-        # train_label_rev = []
-        # weight_labels = []
-        # for s in train:
-        #     frames = constants.GOOD_FRAMES[s]
-        #     train_for.extend([f'data/raw/{s}/{s}_{str(i).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #     train_rev.extend([f'data/raw/{s}/{s}_{str(i-1).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #     train_label_for.extend([f'data/predict_cleaned/{options.temporal}/{s}/{s}_{str(i).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #     train_label_rev.extend([f'data/predict_cleaned/{options.temporal}/{s}/{s}_{str(i-1).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #     weight_labels.extend(glob.glob(f'data/labels/{s}/{s}_{constants.LABELED_FRAME[s]}_{organ}.nii.gz'))
-        # train_gen = AugmentGenerator(train_for + train_rev,
-        #                              label_files=train_label_for + train_label_rev,
-        #                              concat_files=[train_rev + train_for, train_label_rev + train_label_for],
-        #                              label_types=label_types,
-        #                              load_files=options.load_files)
-        # weights = util.get_weights(weight_labels)
+        # train_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in train},
+        #                           'data/raw/{s}/{s}_{n}.nii.gz',
+        #                           f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz',
+        #                           label_types=label_types,
+        #                           load_files=options.load_files,
+        #                           augment=True)
+        # logging.info(f'  Training generator with {len(train_gen)} samples.')
+        # weights = util.get_weights(train_gen.labels)
 
         # val_gen = None
         # if not options.skip_training and options.validate:
-        #     val_for = []
-        #     val_rev = []
-        #     val_label_for = []
-        #     val_label_rev = []
-        #     for s in val:
-        #         frames = constants.GOOD_FRAMES[s]
-        #         val_for.extend([f'data/raw/{s}/{s}_{str(i).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #         val_rev.extend([f'data/raw/{s}/{s}_{str(i-1).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #         val_label_for.extend([f'data/predict_cleaned/{options.temporal}/{s}/{s}_{str(i).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #         val_label_rev.extend([f'data/predict_cleaned/{options.temporal}/{s}/{s}_{str(i-1).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #     val_gen = VolumeGenerator(val_for + val_rev,
-        #                               label_files=val_label_for + val_label_rev,
-        #                               concat_files=[val_rev + val_for, val_label_rev + val_label_for],
-        #                               label_types=label_types,
-        #                               load_files=options.load_files)
+        #     val_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in val},
+        #                             'data/raw/{s}/{s}_{n}.nii.gz',
+        #                             f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz',
+        #                             label_types=label_types,
+        #                             load_files=options.load_files,
+        #                             resize=True)
+        #     logging.info(f'  Validation generator with {len(val_gen)} samples.')
 
-        # if not options.predict_all:
-        #     test_for = []
-        #     test_rev = []
-        #     test_label_for = []
-        #     test_label_rev = []
-        #     for s in test:
-        #         frames = constants.GOOD_FRAMES[s]
-        #         test_for.extend([f'data/raw/{s}/{s}_{str(i).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #         test_rev.extend([f'data/raw/{s}/{s}_{str(i-1).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #         test_label_for.extend([f'data/predict_cleaned/{options.temporal}/{s}/{s}_{str(i).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #         test_label_rev.extend([f'data/predict_cleaned/{options.temporal}/{s}/{s}_{str(i-1).zfill(4)}.nii.gz' for i in frames if i != 0])
-        #     pred_gen = VolumeGenerator(test_for + test_rev, tile_inputs=True, load_files=options.load_files)
-        #     test_gen = VolumeGenerator(test_for + test_rev,
-        #                                label_files=test_label_for + test_label_rev,
-        #                                concat_files=[test_rev + test_for, test_label_rev + test_label_for],
-        #                                label_types=label_types,
-        #                                load_files=options.load_files)
+        # if options.predict_all:
+        #     pred_gen = DataGenerator({s: np.arange(n) for _, (s, n) in enumerate(constants.SEQ_LENGTH.items())},
+        #                              'data/raw/{s}/{s}_{n}.nii.gz',
+        #                              load_files=False,
+        #                              tile_inputs=True)
+        #     logging.info(f'  Prediction generator with {len(pred_gen)//8} samples.')
+        # else:
+        #     pred_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in test},
+        #                              'data/raw/{s}/{s}_{n}.nii.gz',
+        #                              tile_inputs=True)
+        #     logging.info(f'  Prediction generator with {len(pred_gen)//8} samples.')
+        #     test_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in test},
+        #                              'data/raw/{s}/{s}_{n}.nii.gz',
+        #                              f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz',
+        #                              label_types=label_types,
+        #                              load_files=options.load_files,
+        #                              resize=True)
+        #     logging.info(f'  Testing generator with {len(test_gen)} samples.')
 
         # logging.info('Creating model.')
         # shape = constants.SHAPE[:-1] + (3,)
