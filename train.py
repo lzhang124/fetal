7b71279b7f3ecc7285efb3d81cc2975148c6e5e6
Larry Zhang <lzhang124@gmail.com>
2019-02-21 14:53:03 -0500

    input file format

--- train.py
+++ train.py
@@ -69,73 +69,12 @@ def main(options):
 
     organ = 'all_brains' if options.organ == 'brains' else options.organ
 
-    if options.good_frames:
+    if options.temporal:
         logging.info('Temporal model.')
-        # logging.info('Splitting data.')
-        # samples = list(constants.GOOD_FRAMES.keys())
-        # n = len(samples)
-        # shuffled = np.random.permutation(samples)
-        # if options.validate:
-        #     train = shuffled[:(2*n)//3]
-        #     val = shuffled[(2*n)//3:(5*n)//6]
-        #     test = shuffled[(5*n)//6:]
-        # else:
-        #     train = shuffled[:(9*n)//10]
-        #     test = shuffled[(9*n)//10:]
-
-        # logging.info('Creating data generators.')
-        # label_types = LABELS[options.model]
-        # train_gen = DataGenerator({s: constants.GOOD_FRAMES[s] for s in train},
-        #                           f'data/predict_cleaned/{options.temporal}/{{s}}/{{s}}_{{n}}.nii.gz',
-        #                           f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz',
-        #                           label_types=label_types,
-        #                           load_files=options.load_files,
-        #                           augment=True)
-        # logging.info(f'  Training generator with {len(train_gen)} samples.')
-        # weights = util.get_weights(train_gen.labels)
-
-        # val_gen = None
-        # if not options.skip_training and options.validate:
-        #     val_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in val},
-        #                             'data/raw/{s}/{s}_{n}.nii.gz',
-        #                             f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz',
-        #                             label_types=label_types,
-        #                             load_files=options.load_files,
-        #                             resize=True)
-        #     logging.info(f'  Validation generator with {len(val_gen)} samples.')
-
-        # if options.predict_all:
-        #     pred_gen = DataGenerator({s: np.arange(n) for _, (s, n) in enumerate(constants.SEQ_LENGTH.items())},
-        #                              'data/raw/{s}/{s}_{n}.nii.gz',
-        #                              load_files=False,
-        #                              tile_inputs=True)
-        #     logging.info(f'  Prediction generator with {len(pred_gen)//8} samples.')
-        # else:
-        #     pred_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in test},
-        #                              'data/raw/{s}/{s}_{n}.nii.gz',
-        #                              tile_inputs=True)
-        #     logging.info(f'  Prediction generator with {len(pred_gen)//8} samples.')
-        #     test_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in test},
-        #                              'data/raw/{s}/{s}_{n}.nii.gz',
-        #                              f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz',
-        #                              label_types=label_types,
-        #                              load_files=options.load_files,
-        #                              resize=True)
-        #     logging.info(f'  Testing generator with {len(test_gen)} samples.')
-
-        # logging.info('Creating model.')
-        # shape = constants.SHAPE[:-1] + (3,)
-        # model = MODELS[options.model](shape, name=options.name, filename=options.model_file, weights=weights)
-    else:
-        logging.info('Non-temporal model.')
         logging.info('Splitting data.')
-        if options.good_frames:
-            samples = list(constants.GOOD_FRAMES.keys())
-            n = len(samples)
-            shuffled = np.random.permutation(samples)
-        else:
-            n = len(constants.LABELED_SAMPLES)
-            shuffled = np.random.permutation(constants.LABELED_SAMPLES)
+        samples = list(constants.GOOD_FRAMES.keys())
+        n = len(samples)
+        shuffled = np.random.permutation(samples)
         assert np.sum(options.split) < 1, 'Split is greater than 1.'
         train_split = int(options.split[0] * n)
         val_split = int(np.sum(options.split) * n)
@@ -181,6 +120,73 @@ def main(options):
         test_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in test},
                                  'data/raw/{s}/{s}_{n}.nii.gz',
                                  f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz',
+                                 label_types=label_types,
+                                 load_files=options.load_files,
+                                 resize=True)
+        logging.info(f'  Testing generator with {len(test_gen)} samples.')
+
+        logging.info('Creating model.')
+        shape = constants.SHAPE[:-1] + (3,)
+        model = MODELS[options.model](shape, name=options.name, filename=options.model_file, weights=weights)
+    else:
+        logging.info('Non-temporal model.')
+        logging.info('Splitting data.')
+        input_file_format = 'data/raw/{s}/{s}_{n}.nii.gz'
+        if options.good_frames:
+            samples = list(constants.GOOD_FRAMES.keys())
+            n = len(samples)
+            shuffled = np.random.permutation(samples)
+            label_file_format = f'data/predict_cleaned/{options.good_frames}/{{s}}/{{s}}_{{n}}.nii.gz'
+        else:
+            n = len(constants.LABELED_SAMPLES)
+            shuffled = np.random.permutation(constants.LABELED_SAMPLES)
+            input_file_format = 'data/raw/{s}/{s}_{n}.nii.gz'
+            label_file_format = f'data/labels/{{s}}/{{s}}_{{n}}_{organ}.nii.gz'
+        assert np.sum(options.split) < 1, 'Split is greater than 1.'
+        train_split = int(options.split[0] * n)
+        val_split = int(np.sum(options.split) * n)
+        train = shuffled[:train_split]
+        val = shuffled[train_split:val_split]
+        test = shuffled[val_split:]
+        assert len(test) > 0, 'No test data.'
+
+        logging.info('Creating data generators.')
+        label_types = LABELS[options.model]
+        train_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in train},
+                                  input_file_format,
+                                  label_file_format,
+                                  label_types=label_types,
+                                  load_files=options.load_files,
+                                  augment=True)
+        logging.info(f'  Training generator with {len(train_gen)} samples.')
+        weights = util.get_weights(train_gen.labels)
+
+        val_gen = None
+        if not options.skip_training and len(val) > 0:
+            val_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in val},
+                                    input_file_format,
+                                    label_file_format,
+                                    label_types=label_types,
+                                    load_files=options.load_files,
+                                    resize=True)
+            logging.info(f'  Validation generator with {len(val_gen)} samples.')
+
+        if options.predict_all:
+            pred_gen = DataGenerator({s: np.arange(n) for _, (s, n) in enumerate(constants.SEQ_LENGTH.items())},
+                                     'data/raw/{s}/{s}_{n}.nii.gz',
+                                     load_files=False,
+                                     tile_inputs=True)
+            logging.info(f'  Prediction generator with {len(pred_gen)//8} samples.')
+        else:
+            pred_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in test},
+                                     input_file_format
+                                     load_files=options.load_files,
+                                     tile_inputs=True)
+            logging.info(f'  Prediction generator with {len(pred_gen)//8} samples.')
+        
+        test_gen = DataGenerator({s: [constants.LABELED_FRAME[s]] for s in test},
+                                 input_file_format,
+                                 label_file_format,
                                  label_types=label_types,
                                  load_files=options.load_files,
                                  resize=True)
