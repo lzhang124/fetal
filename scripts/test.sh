fe966524b87ec210ad8b43aef736d5c48bb90fef
Larry Zhang <lzhang124@gmail.com>
2019-03-04 07:34:01 -0500

    volmes

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@ declare -a arr=("052218S" "052218L" "031616" "062515" "051215" "043015" "052516"
 
 for i in "${arr[@]}"
 do
-    srun -p gpu -t 20:00:00 --mem-per-cpu 16 --gres=gpu:1 python make_gif.py unet_frames_${i} --sample ${i} &
+    srun -p gpu -t 20:00:00 --mem-per-cpu 16 --gres=gpu:1 python volume_plot.py unet_frames_${i} --sample ${i} &
 done
